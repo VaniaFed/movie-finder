f3bb37b83af97754dd73c67db05a60e969c7e43e
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-16 18:12:36 +0300

    add footer and header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,10 @@ import { MovieSearch } from 'Components/ui/MovieSearch/';
 export const App = () => {
     return (
         <div className="container">
+            <header>netfixroulette</header>
             <MovieSearch />
             {/* <MoviePage /> */}
+            <footer>netfixroulette</footer>
         </div>
     );
 };
