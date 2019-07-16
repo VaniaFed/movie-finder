44ddbb07d17f03da517e05b32dca68acaf7eaf17
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-16 18:14:15 +0300

    add test movies

--- src/components/ui/MovieSearch/index.jsx
+++ src/components/ui/MovieSearch/index.jsx
@@ -1,17 +1,29 @@
 import React, { Fragment } from 'react';
 import { MoviesList } from 'Components/ui/MoviesList';
+import { v4 } from 'uuid';
 import { Search } from './Search';
 
 export const MovieSearch = () => {
     const movies = [
         {
-            label: 'movei1'
+            label: 'movei1',
+            id: v4()
         },
         {
-            label: 'movei2'
+            label: 'movei2',
+            id: v4()
         },
         {
-            label: 'movei3'
+            label: 'movei1',
+            id: v4()
+        },
+        {
+            label: 'movei2',
+            id: v4()
+        },
+        {
+            label: 'movei3',
+            id: v4()
         }
     ];
     return (
