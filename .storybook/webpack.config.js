de07ef483fb0ffc7f3b310dbe114d08d4c22f5be
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-17 19:27:02 +0300

    refactor storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,43 +1,11 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const merge = require('webpack-merge');
 
-module.exports = {
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.(png|woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
-                loader: 'url?limit=10000'
-            },
-            {
-                test: /\.css$/,
-                loader:
-                    'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&sourceMap&-minimize'
-            },
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader!postcss-loader',
-                include: path.join(__dirname, 'node_modules'),
-                exclude: /flexboxgrid/
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                    { loader: 'sass-loader' }
-                ]
-            }
-        ]
-    }
+const baseConfig = require('../webpack/webpack-config');
+const devConfig = require('../webpack/webpack-dev-config');
+const aliasConfig = require('../webpack/webpack-alias-config.js');
+
+const config = merge([baseConfig, devConfig, aliasConfig]);
+
+module.exports = async ({ config, mode }) => {
+    return { ...config };
 };
