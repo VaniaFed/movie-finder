ed5592f57fe007ddba5c966c76d8d34e1ba19d05
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-19 17:32:25 +0300

    separate styles

--- src/components/ui/SearchInput/index.jsx
+++ src/components/ui/SearchInput/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SearchInput.sass';
 
 export const SearchInput = ({ className }) => (
     <input className="search-input" type="text" placeholder="Game of Thrones" />
