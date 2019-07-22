6cd190509407ede55d81c833b06a7ad4056ee7f3
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-22 17:18:28 +0300

    fix comments for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,6 +7,8 @@ function loadStories() {
     require('../src/components/ui/NotFound/NotFound.stories');
     require('../src/components/ui/ToggleContainer/ToggleContainer.stories');
     require('../src/components/ui/ToggleButton/ToggleButton.stories');
+
+    // requires alias for the following stories:
     // require('../src/components/ui/SearchBlock/SearchBlock.stories');
     // require('../src/components/ui/Movies/Movies.stories');
 }
