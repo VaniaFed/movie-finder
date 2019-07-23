12e869bdeaaec9c13f9fb6804ee1af193189fdbf
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-23 14:50:53 +0300

    add button to SearchInput

--- src/components/ui/SearchInput/index.jsx
+++ src/components/ui/SearchInput/index.jsx
@@ -4,9 +4,12 @@ import classNames from 'classnames';
 import './searchInput.scss';
 
 export const SearchInput = ({ className }) => (
-    <input
-        className={classNames('search-input', className)}
-        type="text"
-        placeholder="Game of Thrones"
-    />
+    <div className={classNames('search-input__wrapper', className)}>
+        <input
+            className={classNames('search-input', className)}
+            type="text"
+            placeholder="Game of Thrones"
+        />
+        <button className="search-input__btnIcon" type="button" />
+    </div>
 );
