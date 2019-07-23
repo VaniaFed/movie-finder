38fe8685f3e12116974520654ce76eec50953fe2
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-23 19:08:20 +0300

    add modificator noBg for ToggleButton and name

--- src/components/ui/ToggleButton/index.jsx
+++ src/components/ui/ToggleButton/index.jsx
@@ -1,20 +1,24 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './toggleButton.scss';
 
-export const ToggleButton = memo(({ label }) => {
+export const ToggleButton = memo(({ label, className, checked, name }) => {
+    const resultClass = classNames(className, 'toggle-button');
     return (
-        <label className="toggle-button">
+        <label className={resultClass}>
             <input
                 className="toggle-button__input"
                 type="radio"
-                name="toggle"
+                defaultChecked={checked || ''}
+                name={name}
             />
             <span className="toggle-button__text">{label}</span>
         </label>
     );
 });
 ToggleButton.propTypes = {
-    label: PropTypes.string.isRequired
+    label: PropTypes.string.isRequired,
+    checked: PropTypes.string
 };
