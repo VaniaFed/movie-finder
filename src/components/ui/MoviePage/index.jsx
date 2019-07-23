4b095d7814787ccb77089e99c8c5410c2177cb48
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-23 18:08:17 +0300

    add moviePage

---
+++ src/components/ui/MoviePage/index.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import { MovieBlock } from 'Components/ui/MovieBlock/';
+import { Movies } from 'Components/ui/Movies/';
+
+import './MoviePage.scss';
+
+export const MoviePage = ({ movies }) => {
+    return (
+        <Fragment>
+            <MovieBlock />
+            <div className="moviePage-info">
+                <span className="moviePage-info__text">Films by</span>
+                <span className="moviePage-info__genre">Drama genre</span>
+            </div>
+            <Movies movies={movies} />
+        </Fragment>
+    );
+};
