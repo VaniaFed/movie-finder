961337886ea3c565ec43b80429f9c3f03d32b11d
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-23 16:28:30 +0300

    add global styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,14 +1,15 @@
 import { configure } from '@storybook/react';
+import '../src/global.scss';
 
 function loadStories() {
     require('../src/components/ui/Movie/Movie.stories');
-    require('../src/components/ui/SearchInput/SearchInput.stories');
     require('../src/components/ui/MovieInfo/MovieInfo.stories');
     require('../src/components/ui/NotFound/NotFound.stories');
     require('../src/components/ui/ToggleContainer/ToggleContainer.stories');
     require('../src/components/ui/ToggleButton/ToggleButton.stories');
 
     // requires alias for the following stories:
+    // require('../src/components/ui/SearchInput/SearchInput.stories');
     // require('../src/components/ui/SearchBlock/SearchBlock.stories');
     // require('../src/components/ui/Movies/Movies.stories');
 }
