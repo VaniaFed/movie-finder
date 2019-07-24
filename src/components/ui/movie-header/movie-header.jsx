f517a0ad1b5e4e5d4297ea8fdc61b4554bbbd9ef
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-24 15:51:27 +0300

    rename MovieHeader -> movie-header

--- src/components/ui/movie-header/movie-header.jsx
+++ src/components/ui/movie-header/movie-header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 import { Logo } from 'Components/ui/logo';
 import { Button } from 'Components/ui/button';
-import './MovieHeader.scss';
+import './movie-header.scss';
 
 export const MovieHeader = ({ className }) => {
-    const resultClass = classNames(className, 'movieHeader');
+    const resultClass = classNames(className, 'movie-header');
     return (
         <div className={resultClass}>
             <Logo>netfixroulette</Logo>
-            <Button className="movieHeader__searchButton button_reverse">
+            <Button className="movie-header__search-button button_reverse">
                 Search
             </Button>
         </div>
