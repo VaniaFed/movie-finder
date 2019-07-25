dc7e9129a7dfcac939235287cfcc5294db695449
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-25 15:35:32 +0300

    add error-boundary

---
+++ src/components/ui/error-boundary/error-boundary.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, info) {
+        console.log('error: ', error);
+        console.log('error: ', info);
+    }
+
+    render() {
+        const { hasError } = this.state;
+        if (hasError) {
+            // You can render any custom fallback UI
+            return <h1>Something went wrong.</h1>;
+        }
+
+        return this.props.children;
+    }
+}
