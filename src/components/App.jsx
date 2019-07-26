4bcb4fdb247f357e97215c08e71eb8d3bf95ce1a
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-26 17:43:52 +0300

    move error-boundary to containers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { SearchPage } from 'components/search-page/';
 import { MoviePageSmart } from 'containers/movie-page-smart';
 import { Logo } from 'components/logo';
-import { ErrorBoundary } from 'components/error-boundary';
+import { ErrorBoundary } from 'containers/error-boundary';
 
 export const App = () => {
     return (
