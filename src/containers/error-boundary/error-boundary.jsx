dc50e79336cec2c29963327335e18aa0ed61e772
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-29 19:36:46 +0300

    refactor error-boundary

--- src/containers/error-boundary/error-boundary.jsx
+++ src/containers/error-boundary/error-boundary.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 
 export class ErrorBoundary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+    state = { hasError: false };
 
-    static getDerivedStateFromError(error) {
-        return { hasError: true };
-    }
-
-    componentDidCatch(error, info) {
-        console.log('error: ', error);
-        console.log('error: ', info);
+    componentDidCatch() {
+        this.setState({ hasError: true });
     }
 
     render() {
         const { hasError } = this.state;
+        const { children } = this.props;
+
         if (hasError) {
             return <h1>Something went wrong.</h1>;
         }
 
-        return this.props.children;
+        return children;
     }
 }
