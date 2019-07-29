a7e5be8c22feca201d216df14c342ea8dca96485
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-29 19:41:21 +0300

    remove rubbish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { SearchPage } from 'components/search-page/';
-import { MoviePageSmart } from 'containers/movie-page-smart';
-import { Logo } from 'components/logo';
-import { ErrorBoundary } from 'containers/error-boundary';
+import { Button } from 'components/button';
 
 export const App = () => {
     return (
-        <ErrorBoundary>
-            <div className="container">
-                <SearchPage />
-                <MoviePageSmart />
-                <footer>
-                    <Logo>netfixroulette</Logo>
-                </footer>
-            </div>
-        </ErrorBoundary>
+        <div className="container">
+            <Button text="Hello" />
+        </div>
     );
 };
