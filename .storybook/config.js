7af3d30150233358c118cb0d2e2c4ef8c6929179
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-30 16:54:26 +0300

    change order stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,16 +4,17 @@ import '../src/global.scss';
 function loadStories() {
     require('components/movie-page/movie-page.stories');
     require('components/movie-block/movie-block.stories');
+    require('components/movie/movie.stories');
     require('components/movie-loyout/movie-loyout.stories');
     require('components/movie-card/movie-card.stories');
-    require('components/movie/movie.stories');
     require('components/search-page/search-page.stories');
-    require('components/search-input/search-input.stories');
     require('components/search-block/search-block.stories');
+    require('components/search-input/search-input.stories');
+    require('components/search-page-info/search-page-info.stories');
     require('components/not-found/not-found.stories');
-    require('components/toggle-button/toggle-button.stories');
-    require('components/toggle-loyout/toggle-loyout.stories');
     require('containers/toggle-container/toggle-container.stories');
+    require('components/toggle-loyout/toggle-loyout.stories');
+    require('components/toggle-button/toggle-button.stories');
     require('components/button/button.stories');
     require('components/logo/logo.stories');
 }
