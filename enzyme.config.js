cc3770d7d4bd49e0d75f59e910c7f289ec0d4619
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-31 21:28:08 +0300

    fix jest config

--- enzyme.config.js
+++ enzyme.config.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import enzyme from 'enzyme';
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 global.React = React;
--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    setupFiles: ['./enzyme.config.js'],
+    setupFiles: ['<rootDir>/enzyme.config.js'],
     modulePathIgnorePatterns: ['enzyme.config.js'],
     moduleNameMapper: {
+        '^components(.*)$': '<rootDir>/src/components$1',
+        '^containers(.*)$': '<rootDir>/src/containers$1',
+        '^.+\\.js(jsx)$': 'babel-jest',
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
     }
 };
