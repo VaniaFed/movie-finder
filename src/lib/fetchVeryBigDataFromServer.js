758dad440f66c73bf18024330bfeb240d1851fa9
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-01 15:06:08 +0300

    add mock for fetchVeryBigDataFromServer

---
+++ src/lib/fetchVeryBigDataFromServer.js
@@ -0,0 +1,3 @@
+export const fetchVeryBigDataFromServer = () => {
+    console.log('Fetched...');
+};
--- src/lib/math.test.js
+++ src/lib/math.test.js
@@ -1,5 +1,9 @@
 import { sum } from './math';
 
+jest.mock('./fetchVeryBigDataFromServer', () => ({
+    fetchVeryBigDataFromServer: () => console.log('Fetching prevented')
+}));
+
 test('5 sum 3 is equal 8', () => {
     expect(sum(5, 3, 2, -2, 0)).toBe(8);
 });
