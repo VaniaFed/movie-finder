ec1284c79f0dd605bcd8ddf6153f0073813a7482
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-01 16:32:34 +0300

    add footer component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { SearchPageSmart } from 'containers/search-page-smart/';
 import { MoviePageSmart } from 'containers/movie-page-smart';
-import { Logo } from 'components/logo';
 import { ErrorBoundary } from 'containers/error-boundary';
+import { Footer } from 'components/footer';
 
 export const App = () => {
     return (
         <ErrorBoundary>
-            <div className="container">
-                <SearchPageSmart />
-                <MoviePageSmart />
-                <footer>
-                    <Logo text="netfixroulette" />
-                </footer>
-            </div>
+            <SearchPageSmart />
+            <MoviePageSmart />
+            <Footer />
         </ErrorBoundary>
     );
 };
