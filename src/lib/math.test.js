55c696796a21f1d9b482dcbad517607d598f25be
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-01 16:00:50 +0300

    add check quantity call to be 1

--- src/lib/math.test.js
+++ src/lib/math.test.js
@@ -1,9 +1,13 @@
 import { sum } from './math';
+import { fetchVeryBigDataFromServer } from './fetchVeryBigDataFromServer';
 
-jest.mock('./fetchVeryBigDataFromServer', () => ({
-    fetchVeryBigDataFromServer: () => console.log('Fetching prevented')
-}));
+jest.mock('./fetchVeryBigDataFromServer');
+fetchVeryBigDataFromServer.mockImplementation(() => 'Fetching prevented');
 
 test('5 sum 3 is equal 8', () => {
     expect(sum(5, 3, 2, -2, 0)).toBe(8);
+});
+
+test('should fetchVeryBigDataFromServer be called 1 time', () => {
+    expect(fetchVeryBigDataFromServer.mock.calls.length).toBe(1);
 });
