d961db9110e5d8905162fe4467778a51c829602a
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-02 13:04:29 +0300

    refactor; add useEffect, state and move movies with the same genres to MoviePageSmart

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { MovieBlock } from 'components/movie-block/';
-import { MovieLoyout } from 'components/movie-loyout/';
 import { MoviePageInfo } from 'components/movie-page-info';
-import { NotFound } from 'components/not-found';
-import { YetLoader } from 'containers/yet-loader';
 
-export const MoviePage = ({ movie, moviesWithTheSameGenre }) => {
-    const Cap = () => <NotFound caption="No films found" />;
+export const MoviePage = ({ movie }) => {
     return (
         <>
             <MovieBlock movie={movie} />
             <MoviePageInfo genre={movie.genres} />
-            <YetLoader
-                condition={moviesWithTheSameGenre.length > 0}
-                cap={<Cap />}
-            >
-                <MovieLoyout movies={moviesWithTheSameGenre} />
-            </YetLoader>
         </>
     );
 };
 
 MoviePage.propTypes = {
-    movie: PropTypes.object,
-    moviesWithTheSameGenre: PropTypes.objectOf(PropTypes.object)
+    movie: PropTypes.object
 };
--- src/containers/movie-page-smart/movie-page-smart.jsx
+++ src/containers/movie-page-smart/movie-page-smart.jsx
@@ -1,39 +1,37 @@
-import React from 'react';
-import { MoviePage } from 'components/movie-page';
+import React, { useEffect, useState } from 'react';
 import { v4 } from 'uuid';
+import { MoviePage } from 'components/movie-page';
+import { YetLoader } from 'containers/yet-loader';
+import { MovieLoyout } from 'components/movie-loyout/';
+import { NotFound } from 'components/not-found';
 
 export const MoviePageSmart = () => {
-    const movies = [
-        {
-            title: 'movei1',
-            genres: 'action',
-            tagline: 'some tagline',
-            releaseDate: '2014',
-            runtime: 160,
-            overview:
-                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quidem non id numquam nulla, porro itaque laborum ut, minima illum quaerat, iusto dignissimos harum quae? Corrupti sit laboriosam optio magnam!',
-            posterPath:
-                'https://pp.userapi.com/c844521/v844521991/f808b/TCjtAXN8SdA.jpg',
-            id: v4()
-        },
-        {
-            title: 'movei1',
-            genres: 'action',
-            tagline: 'some tagline',
-            releaseDate: '2014',
-            posterPath:
-                'https://pp.userapi.com/c844521/v844521991/f808b/TCjtAXN8SdA.jpg',
-            id: v4()
-        },
-        {
-            title: 'movei1',
-            genres: 'action',
-            tagline: 'some tagline',
-            releaseDate: '2014',
-            posterPath:
-                'https://pp.userapi.com/c844521/v844521991/f808b/TCjtAXN8SdA.jpg',
-            id: v4()
-        }
-    ];
-    return <MoviePage movie={movies[0]} moviesWithTheSameGenre={movies} />;
+    const [moviesWithTheSameGenre, setMoviesWithTheSameGenre] = useState([]);
+    const movie = {
+        title: 'movei1',
+        genres: 'action',
+        tagline: 'some tagline',
+        releaseDate: '2014',
+        runtime: 160,
+        overview:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quidem non id numquam nulla, porro itaque laborum ut, minima illum quaerat, iusto dignissimos harum quae? Corrupti sit laboriosam optio magnam!',
+        posterPath:
+            'https://pp.userapi.com/c844521/v844521991/f808b/TCjtAXN8SdA.jpg',
+        id: v4()
+    };
+    useEffect(() => {
+        setMoviesWithTheSameGenre([movie]);
+    });
+    const Cap = () => <NotFound caption="No films found" />;
+    return (
+        <>
+            <MoviePage movie={movie} />
+            <YetLoader
+                condition={moviesWithTheSameGenre.length > 0}
+                cap={<Cap />}
+            >
+                <MovieLoyout movies={moviesWithTheSameGenre} />
+            </YetLoader>
+        </>
+    );
 };
