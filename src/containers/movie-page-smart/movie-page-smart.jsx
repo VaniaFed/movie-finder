03687e17f784cb5c5b508cc5832319ce3ff5ea1b
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-07 20:04:31 +0300

    add reselect

--- src/containers/movie-page-smart/movie-page-smart.jsx
+++ src/containers/movie-page-smart/movie-page-smart.jsx
@@ -1,17 +1,23 @@
 import React, { memo, useEffect } from 'react';
+import { createSelector } from 'reselect';
 import { connect } from 'react-redux';
+
 import { MoviePage } from 'components/movie-page';
 import { YetLoader } from 'containers/yet-loader';
 import { MovieLayout } from 'components/movie-layout/';
 import { NotFound } from 'components/not-found';
 import { fetchMovieByIdRequest } from 'actions/movie-by-id';
 
-const mapStateToProps = state => {
-    return {
-        movie: state.movie.movie,
-        moviesWithTheSameGenre: state.movies
-    };
-};
+const selectMovies = state => state.movies;
+
+const selectMovie = state => state.movie;
+const mapStateToProps = createSelector(
+    [selectMovie, selectMovies],
+    (movie, movies) => ({
+        movie: movie.movie,
+        moviesWithTheSameGenre: movies
+    })
+);
 
 const mapDispatchToProps = dispatch => {
     return {
@@ -29,7 +35,8 @@ export const MoviePageSmart = connect(
     memo(({ movie, moviesWithTheSameGenre, fetchMovieById }) => {
         useEffect(() => {
             fetchMovieById(5);
-        }, []);
+            console.log(movie);
+        }, movie);
 
         const Cap = () => <NotFound caption="No films found" />;
         const MovieCap = () => <NotFound caption="Film not found" />;
