5d2e024f168ad9f3b9c23fb744d575eedaede3fd
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-10 17:59:41 +0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     setupFiles: ['<rootDir>/enzyme.config.js'],
-    modulePathIgnorePatterns: ['enzyme.config.js'],
+    modulePathIgnorePatterns: ['enzyme.config.js', '\\\\node_modules\\\\'],
     moduleNameMapper: {
         '^components(.*)$': '<rootDir>/src/components$1',
         '^containers(.*)$': '<rootDir>/src/containers$1',
+        '^actions(.*)$': '<rootDir>/src/actions$1',
+        '^constants.js(.*)$': '<rootDir>/src/constants.js$1',
+        '^selectors(.*)$': '<rootDir>/src/selectors$1',
+        '^services(.*)$': '<rootDir>/src/services$1',
+        '^sagas(.*)$': '<rootDir>/src/sagas$1',
         '^.+\\.js(jsx)$': 'babel-jest',
-        '\\.(css|less|scss|sass)$': 'jest-transform-stub',
-        '^selectors(.*)$': ['<rootDir>/src/selectors$1'],
-        '^utils(.*)$': ['<rootDir>/src/utils$1'],
-        '^services(.*)$': ['<rootDir>/src/services$1'],
-
-        '^sagas(.*)$': ['<rootDir>/src/sagas$1']
+        '\\.(css|less|scss|sass)$': 'jest-transform-stub'
     }
 };
