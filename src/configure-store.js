e8789dfc5c5344b0e73b2af407f612b01f8056f2
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-12 19:33:30 +0300

    fix devtools for testing

--- src/configure-store.js
+++ src/configure-store.js
@@ -11,8 +11,9 @@ export const configureStore = () => {
         rootReducer,
         compose(
             applyMiddleware(sagaMiddleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ &&
-                window.__REDUX_DEVTOOLS_EXTENSION__()
+            window.__REDUX_DEVTOOLS_EXTENSION__
+                ? window.__REDUX_DEVTOOLS_EXTENSION__()
+                : f => f
         )
     );
     sagaMiddleware.run(rootSaga);
