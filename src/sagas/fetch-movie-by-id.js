2878bd7a1142b3cb96115a82ce380ebf91537047
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-12 17:43:28 +0300

    rename getFilmsById to getMovieById

--- src/sagas/fetch-movie-by-id.js
+++ src/sagas/fetch-movie-by-id.js
@@ -3,11 +3,11 @@ import { put, call, takeEvery } from 'redux-saga/effects';
 import { actions } from 'actions';
 
 import { FETCH_MOVIE_BY_ID_REQUEST } from 'constants.js';
-import { getFilmsById } from 'services/getFilmsById';
+import { getMovieById } from 'services/getMovieById';
 
 export function* fetchMovieById({ payload }) {
     try {
-        const response = yield call(getFilmsById, payload);
+        const response = yield call(getMovieById, payload);
         const movie = response;
         yield put(actions.fetchMovieByIdSuccess(movie));
         const genre = movie.genres[0];
--- src/services/getMovieById.js
+++ src/services/getMovieById.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const getFilmsById = async payload => {
+export const getMovieById = async payload => {
     const url = `http://react-cdp-api.herokuapp.com/movies/${payload.id}`;
     const movie = await axios(url).then(res => {
         return res.data;
