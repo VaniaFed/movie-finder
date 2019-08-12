0563be9a94504fb861d8a1c0a749b2bfcdf6609b
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-12 17:35:29 +0300

    refactor getFilms.js

--- src/services/getFilms.js
+++ src/services/getFilms.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-import { stringify } from 'query-string';
 
-export const getFilms = payload => {
-    const url = `http://react-cdp-api.herokuapp.com/movies?${stringify(
-        payload
-    )}`;
-    return axios(url).then(res => {
+export const getFilms = params => {
+    const url = 'http://react-cdp-api.herokuapp.com/movies';
+    return axios.get(url, { params }).then(res => {
         return res.data;
     });
 };
