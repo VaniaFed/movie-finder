7d5be3a6f0057b2ae5edba264d70a58465819e6b
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-13 17:12:30 +0300

    add another autotest

---
+++ autotests/checkSpecificMovieTitleIsCorrect.spec.js
@@ -0,0 +1,19 @@
+import 'regenerator-runtime/runtime';
+
+const puppeteer = require('puppeteer');
+
+test('Quantity of movies should be is more than zero', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:9000/movies/353616');
+    const movieTitleSelector = '[data-text="movie-page__movie-title"]';
+    await page.waitForSelector(movieTitleSelector);
+
+    const movieTitle = await page.evaluate(
+        movieTitleSelector => document.querySelector(movieTitleSelector).innerText,
+        movieTitleSelector
+    );
+
+    browser.close();
+    expect(movieTitle).toBe('Pitch Perfect 3');
+});
