a2d151ef4b0f0ebe61810bb000cd87ef9c117bf5
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-16 13:53:02 +0300

    prevent load sameGenres if genre the same

--- src/sagas/fetch-movies-by-genre.js
+++ src/sagas/fetch-movies-by-genre.js
@@ -1,13 +1,21 @@
-import { put, call, takeEvery } from 'redux-saga/effects';
+import { put, call, takeEvery, select } from 'redux-saga/effects';
 import { actions } from 'actions';
 import { FETCH_MOVIES_BY_GENRE_REQUEST } from 'constants.js';
 import { services } from 'services';
+import { sameGenreListSelector } from 'selectors/same-genre-list-selector';
 
 export function* fetchMoviesByGenre({ payload }) {
     try {
-        const response = yield call(services.getMovies, payload);
-        const movies = response.data;
-        yield put(actions.fetchMoviesByGenreSuccess(movies));
+        const store = yield select();
+        const sameGenreMovies = sameGenreListSelector(store);
+        if (
+            sameGenreMovies.length === 0 ||
+            sameGenreMovies[0].genres[0] !== payload.search
+        ) {
+            const response = yield call(services.getMovies, payload);
+            const movies = response.data;
+            yield put(actions.fetchMoviesByGenreSuccess(movies));
+        }
     } catch (error) {
         yield put(actions.fetchMoviesByGenreError(error.message));
     }
