fde63037e9d4f224b521a4367662c7f4d2f3d370
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-28 15:12:50 +0300

    remove comments

--- src/lib/is-map.js
+++ src/lib/is-map.js
@@ -1,26 +1,3 @@
-// export const applyEverythingIsEverythingIsOkay = (urlData, data, actions) => {
-//     if (
-//         (urlData.search !== data.searchValue && urlData.search) ||
-//         (urlData.searchFilter !== data.searchFilter && urlData.searchFilter) ||
-//         (urlData.sortFilter !== data.sortFilter && urlData.sortFilter)
-//     ) {
-//         actions.fetchMoviesByData(
-//             urlData.search,
-//             urlData.searchFilter,
-//             urlData.sortFilter
-//         );
-//     }
-//     if (urlData.search !== data.searchValue && urlData.search) {
-//         actions.changeSearchValue(urlData.search);
-//     }
-//     if (urlData.searchFilter !== data.searchFilter && urlData.searchFilter) {
-//         actions.changeSearchFilter(urlData.searchFilter);
-//     }
-//     if (urlData.sortFilter !== data.sortFilter && urlData.sortFilter) {
-//         actions.changeSortFilter(urlData.sortFilter);
-//     }
-// };
-
 export const isMap = (data, target, changeOption = []) => {
     const dataList = Object.entries(data);
     const targetList = Object.entries(target);
