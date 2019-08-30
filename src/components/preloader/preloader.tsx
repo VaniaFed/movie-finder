c3efe0dd7634c2b5610db1a4108d6c5309a4a864
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-30 11:14:49 +0300

    add preloader

---
+++ src/components/preloader/preloader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Preloader = () => {
+    return (
+        <div class="preloader">
+            <img src="/static/img/Spinner-1s-200px.gif" class="indeterminate" />
+            <h1>loading</h1>
+        </div>
+    );
+};
