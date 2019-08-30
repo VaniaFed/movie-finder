790b161f7ecb985f3c86ddaa78f2ad17699319a1
Ivan Fediakov <ivan_fediakov@epam.com>
2019-08-30 11:45:41 +0300

    add preloader for search page

--- src/containers/search-page-smart/search-page-smart.jsx
+++ src/containers/search-page-smart/search-page-smart.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
-import { withRouter } from 'next/router';
 import { parse } from 'query-string';
 import { SearchPage } from 'components/search-page';
 import { actions } from 'actions';
-import { listSelector } from 'selectors/list-selector';
 import { isMap } from 'lib/is-map';
 import { pushToHistory } from 'lib/push-to-history';
 import { searchValueSelector } from 'selectors/search-value-selector';
 import { searchFilterSelector } from 'selectors/search-filter-selector';
 import { sortFilterSelector } from 'selectors/sort-filter-selector';
+import { listSelector } from 'selectors/list-selector';
 
 const mapStateToProps = createSelector(
     listSelector,
@@ -55,61 +54,69 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export const SearchPageSmart = withRouter(
-    connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(({ list, controlsData, dispatches }) => {
-        useEffect(() => {
-            const urlData = parse(window.location.search);
-            const inputData = {
-                search: controlsData.search,
-                searchFilter: controlsData.searchFilter,
-                sortFilter: controlsData.sortFilter
-            };
+export const SearchPageSmart = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(({ list, controlsData, dispatches }) => {
+    const [isStartedLoading, setIsStartedLoading] = useState(false);
+    useEffect(() => {
+        const urlData = parse(window.location.search);
+        const inputData = {
+            search: controlsData.search,
+            searchFilter: controlsData.searchFilter,
+            sortFilter: controlsData.sortFilter
+        };
 
-            const shouldFetchMovies = !isMap(urlData, inputData);
-            if (shouldFetchMovies) {
-                dispatches.fetchMoviesByData(
-                    urlData.search,
-                    urlData.searchFilter,
-                    urlData.sortFilter
-                );
-            }
-            const shouldChangeSearch = !isMap(urlData, inputData, ['search']);
-            if (shouldChangeSearch) {
-                dispatches.changeSearchValue(urlData.search);
-            }
+        const shouldFetchMovies = !isMap(urlData, inputData);
+        if (shouldFetchMovies && !isStartedLoading) {
+            setIsStartedLoading(true);
+            dispatches.fetchMoviesByData(
+                urlData.search,
+                urlData.searchFilter,
+                urlData.sortFilter
+            );
+        }
+        const shouldChangeSearch = !isMap(urlData, inputData, ['search']);
+        if (shouldChangeSearch) {
+            dispatches.changeSearchValue(urlData.search);
+        }
 
-            const shouldChangeSearchFilter = !isMap(urlData, inputData, [
-                'searchFilter'
-            ]);
-            if (shouldChangeSearchFilter) {
-                dispatches.changeSearchFilter(urlData.searchFilter);
-            }
+        const shouldChangeSearchFilter = !isMap(urlData, inputData, [
+            'searchFilter'
+        ]);
+        if (shouldChangeSearchFilter) {
+            dispatches.changeSearchFilter(urlData.searchFilter);
+        }
 
-            const shouldChangeSortFilter = !isMap(urlData, inputData, [
-                'sortFilter'
-            ]);
-            if (shouldChangeSortFilter) {
-                dispatches.changeSortFilter(urlData.sortFilter);
-            }
-        });
-        return (
-            <>
-                <SearchPage
-                    movies={list}
-                    searchValue={controlsData.searchValue}
-                    searchFilter={controlsData.searchFilter}
-                    sortFilter={controlsData.sortFilter}
-                    changeSortFilter={sortFilter => {
-                        const historyData = {
-                            sortFilter
-                        };
-                        pushToHistory(historyData);
-                    }}
-                />
-            </>
-        );
-    })
-);
+        const shouldChangeSortFilter = !isMap(urlData, inputData, [
+            'sortFilter'
+        ]);
+        if (shouldChangeSortFilter) {
+            dispatches.changeSortFilter(urlData.sortFilter);
+        }
+    });
+
+    useEffect(() => {
+        if (isStartedLoading && list.length !== 0) {
+            setIsStartedLoading(false);
+        }
+    });
+    return (
+        <>
+            <SearchPage
+                movies={list}
+                searchValue={controlsData.searchValue}
+                searchFilter={controlsData.searchFilter}
+                sortFilter={controlsData.sortFilter}
+                isStartedLoading={isStartedLoading}
+                setIsStartedLoading={setIsStartedLoading}
+                changeSortFilter={sortFilter => {
+                    const historyData = {
+                        sortFilter
+                    };
+                    pushToHistory(historyData);
+                }}
+            />
+        </>
+    );
+});
