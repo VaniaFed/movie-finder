973821c5b6f2f652eaa5fff091e524299ca920a1
Ivan Fediakov <ivan_fediakov@epam.com>
2019-09-06 16:59:53 +0300

    replace mapDispatch and mapState with redux hooks

--- src/containers/movie-page-smart/movie-page-smart.tsx
+++ src/containers/movie-page-smart/movie-page-smart.tsx
@@ -1,6 +1,5 @@
 import React, { memo, useEffect, useState, FC } from 'react';
-import { createSelector } from 'reselect';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { MoviePage } from 'components/movie-page';
 import { Preloader } from 'components/preloader';
 import { YetLoader } from 'containers/yet-loader';
@@ -11,71 +10,56 @@ import { currentSelector } from 'selectors/current-selector';
 import { sameGenreListSelector } from 'selectors/same-genre-list-selector';
 import { Props } from './props';
 
-const mapStateToProps = createSelector(
-    currentSelector,
-    sameGenreListSelector,
-    (current, list) => ({
-        movie: current,
-        moviesWithTheSameGenre: list
-    })
-);
-
-const mapDispatchToProps = dispatch => {
-    return {
-        dispatch,
-        fetchMovieById: id => {
-            dispatch(actions.fetchMovieByIdRequest(id));
-        }
+export const MoviePageSmart: FC<Props> = memo(({ match }: Props) => {
+    const dispatch = useDispatch();
+    const fetchMovieById = id => {
+        dispatch(actions.fetchMovieByIdRequest(id));
     };
-};
 
-export const MoviePageSmart: FC<Props> = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(
-    memo(({ movie, moviesWithTheSameGenre, fetchMovieById, match }: Props) => {
-        const { id } = match.params;
-        const [isStartedLoading, setIsStartedLoading] = useState(false);
-        useEffect(() => {
-            if (!isStartedLoading) {
-                setIsStartedLoading(true);
-                fetchMovieById(id);
-            }
-        }, [id]);
+    const movie = useSelector(currentSelector);
+    const moviesWithTheSameGenre = useSelector(sameGenreListSelector);
 
-        useEffect(() => {
-            if (
-                'genres' in movie &&
-                moviesWithTheSameGenre.length !== 0 &&
-                isStartedLoading
-            ) {
-                setIsStartedLoading(false);
-            }
-        }, [movie, moviesWithTheSameGenre, isStartedLoading]);
+    const { id } = match.params;
+    const [isStartedLoading, setIsStartedLoading] = useState(false);
+    useEffect(() => {
+        if (!isStartedLoading) {
+            setIsStartedLoading(true);
+            fetchMovieById(id);
+        }
+    }, [id]);
 
-        const FilmsNotFound = () => <NotFound caption="Film(s) not found" />;
-        return (
-            <>
-                {isStartedLoading ? (
-                    <Preloader />
-                ) : (
-                    <YetLoader
-                        condition={
-                            typeof movie !== 'undefined' &&
-                            'genres' in movie &&
-                            typeof moviesWithTheSameGenre !== 'undefined' &&
-                            moviesWithTheSameGenre.length !== 0
-                        }
-                        cap={<FilmsNotFound />}
-                        content={() => (
-                            <>
-                                <MoviePage movie={movie} />
-                                <MovieLayout movies={moviesWithTheSameGenre} />
-                            </>
-                        )}
-                    />
-                )}
-            </>
-        );
-    })
-);
+    useEffect(() => {
+        if (
+            'genres' in movie &&
+            moviesWithTheSameGenre.length !== 0 &&
+            isStartedLoading
+        ) {
+            setIsStartedLoading(false);
+        }
+    }, [movie, moviesWithTheSameGenre, isStartedLoading]);
+
+    const FilmsNotFound = () => <NotFound caption="Film(s) not found" />;
+    return (
+        <>
+            {isStartedLoading ? (
+                <Preloader />
+            ) : (
+                <YetLoader
+                    condition={
+                        typeof movie !== 'undefined' &&
+                        'genres' in movie &&
+                        typeof moviesWithTheSameGenre !== 'undefined' &&
+                        moviesWithTheSameGenre.length !== 0
+                    }
+                    cap={<FilmsNotFound />}
+                    content={() => (
+                        <>
+                            <MoviePage movie={movie} />
+                            <MovieLayout movies={moviesWithTheSameGenre} />
+                        </>
+                    )}
+                />
+            )}
+        </>
+    );
+});
