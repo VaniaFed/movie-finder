28c4ab2511fd07b31aef1cc541b9878a98a9b72b
Ivan Fediakov <ivan_fediakov@epam.com>
2019-09-09 21:03:43 +0300

    refactor: redux hooks

--- src/containers/smart-search-block/smart-search-block.tsx
+++ src/containers/smart-search-block/smart-search-block.tsx
@@ -1,78 +1,51 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { pushToHistory } from 'lib/push-to-history';
-import { createSelector } from 'reselect';
 import { actions } from 'actions';
 import { SearchBlock } from 'components/search-block';
 import { searchValueSelector } from 'selectors/search-value-selector';
 import { searchFilterSelector } from 'selectors/search-filter-selector';
 
-const mapStateToProps = createSelector(
-    searchValueSelector,
-    searchFilterSelector,
-    (searchValue, searchFilter) => {
-        return {
-            searchValue,
-            searchFilter
-        };
-    }
-);
+export const SmartSearchBlock = ({ sortFilter, setIsStartedLoading }) => {
+    const searchValue = useSelector(searchValueSelector);
+    const searchFilter = useSelector(searchFilterSelector);
 
-const mapDispatchToProps = dispatch => {
-    return {
-        dispatch,
-        fetchMoviesByData: (searchValue, searchFilter, sortFilter) => {
-            dispatch(
-                actions.fetchMoviesByDataRequest(
-                    searchValue,
-                    searchFilter,
-                    sortFilter
-                )
-            );
-        },
-        changeSearchValue: searchValue => {
-            dispatch(actions.setSearchValue(searchValue));
-        },
-        changeSortFilter: sortFilter => {
-            dispatch(actions.sortFilter(sortFilter));
-        }
-    };
-};
-
-export const SmartSearchBlock = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(
-    ({
-        searchValue,
-        searchFilter,
-        sortFilter,
-        changeSearchValue,
-        fetchMoviesByData,
-        setIsStartedLoading
-    }) => {
-        // group
-        return (
-            <SearchBlock
-                searchValue={searchValue}
-                onSearch={searchValue => {
-                    setIsStartedLoading(true);
-                    fetchMoviesByData(searchValue, searchFilter, sortFilter);
-                    pushToHistory({
-                        search: searchValue,
-                        searchFilter,
-                        sortFilter
-                    });
-                }}
-                onInput={searchValue => {
-                    changeSearchValue(searchValue);
-                    pushToHistory({ search: searchValue });
-                }}
-                changeSearchFilter={searchFilter => {
-                    pushToHistory({ searchFilter });
-                }}
-                searchFilter={searchFilter}
-            />
+    const dispatch = useDispatch();
+    const fetchMoviesByData = (searchValue, searchFilter, sortFilter) => {
+        dispatch(
+            actions.fetchMoviesByDataRequest(
+                searchValue,
+                searchFilter,
+                sortFilter
+            )
         );
-    }
-);
+    };
+    const changeSearchValue = searchValue => {
+        dispatch(actions.setSearchValue(searchValue));
+    };
+    const findMovies = searchValue => {
+        setIsStartedLoading(true);
+        fetchMoviesByData(searchValue, searchFilter, sortFilter);
+        pushToHistory({
+            search: searchValue,
+            searchFilter,
+            sortFilter
+        });
+    };
+    return (
+        <SearchBlock
+            searchValue={searchValue}
+            onSearch={searchValue => {
+                findMovies(searchValue);
+            }}
+            onInput={value => {
+                changeSearchValue(value);
+                pushToHistory({ search: value });
+            }}
+            changeSearchFilter={searchFilter => {
+                pushToHistory({ searchFilter });
+            }}
+            searchFilter={searchFilter}
+        />
+    );
+};
