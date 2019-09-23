e2b232db9c6cfdf7480718646b71d9d5057a2551
VaniaFed <ztankist@gmail.com>
2019-09-23 17:10:58 +0300

    refactor: add type

--- src/containers/smart-movie-header/smart-movie-header.tsx
+++ src/containers/smart-movie-header/smart-movie-header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { stringify } from 'querystring';
+import { stringify, ParsedUrlQueryInput } from 'querystring';
 import { MovieHeader } from 'components/movie-header';
 import { searchDataSelector } from 'selectors/search-data-selector';
 import { ControlsData } from 'types/index';
@@ -11,6 +11,6 @@ interface Props {
 export const SmartMovieHeader = ({ className }: Props) => {
     const controlsData: ControlsData = useSelector(searchDataSelector);
 
-    const urlParams = stringify(controlsData);
+    const urlParams = stringify((controlsData as any) as ParsedUrlQueryInput);
     return <MovieHeader className={className} urlParams={urlParams} />;
 };
