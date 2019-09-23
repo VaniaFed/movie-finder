fae5b69def935451913394220f871ff841aed060
VaniaFed <ztankist@gmail.com>
2019-09-23 17:12:42 +0300

    refactor: define CLIENT_SIDE_RENDERING in constants.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,1 @@
-// export { config } from 'src/config';
-// const store = config.__DEV__
-//     ? require('./configure-store-dev')
-//     : require('./configure-store-prod');
-
-// export default store;
-
 export { configureStore } from './configure-store-prod';
