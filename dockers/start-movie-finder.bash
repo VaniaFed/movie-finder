51a229976c10819537982b22c7e9cc7f48ff8b0f
VaniaFed <ztankist@gmail.com>
2019-09-27 18:14:51 +0300

    fix: some bit fixes

--- dockers/start-movie-finder.bash
+++ dockers/start-movie-finder.bash
@@ -1,3 +1,3 @@
 docker stop movie-finder-nginx
 docker rm movie-finder-nginx
-docker run -d -p 8080:80 --name movie-finder-nginx movie-finder-nginx
+docker run -it -p 8080:80 --name movie-finder-nginx movie-finder-nginx
